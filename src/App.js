2f927ab872515628cf58bb1ceffbc7a12582fa36
Ishan Manandhar <ishan02016@gmail.com>
2020-07-29 22:00:13 +0545

    added some code for animation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import doodle from './assets/doodle.svg';
+import { TimelineLite, Power2 } from 'gsap';
 
 function App() {
+  let t1 = new TimelineLite({ delay: 0.8 });
+
+  useEffect(() => {
+    t1.from('.bannerMain-rgt', 3, { y: 1280, ease: Power2.easeOut }, 'Start');
+    t1.staggerFrom(
+      '.text',
+      1,
+      { y: 56, ease: Power2.easeIn, delay: 0.1, opacity: 0 },
+      0.15,
+      'Start'
+    )
+      .from(
+        '.bannerMain-lft__inner',
+        1,
+        { y: 20, opacity: 0, ease: Power2.easeIn },
+        1.4
+      )
+      .from('.btn-primary', 1, { y: 20, opacity: 0, ease: Power2.easeIn }, 1.4);
+  });
+
   return (
     <div className="App container">
       <section className="bannerMain">
         <div className="bannerMain-lft">
-          <h2>Go paperless <br/>with our App</h2>
-          <p>We provide seemeless integration with <br/> multiple devices so that you can take <br/> notes even better.</p>
+          <div className="bannerMain-lft__inner">
+          <h2>
+            <span className="text">Go paperless </span><br />
+            <span className="text">with our App </span>
+          </h2>
+          <p className="textAnimation">
+            We provide seemeless integration with <br /> multiple devices so
+            that you can take <br /> notes even better.
+          </p>
+          </div>
           <button class="btn-primary">request access</button>
         </div>
         <div className="bannerMain-rgt">
@@ -20,5 +49,3 @@ function App() {
 }
 
 export default App;
-
-
