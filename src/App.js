3af17fdd5e7b35666f9beaea823f6cc85601b58c
Ishan Manandhar <ishan02016@gmail.com>
2020-07-30 10:11:50 +0545

    added some code for animation

--- src/App.js
+++ src/App.js
@@ -15,35 +15,58 @@ function App() {
       0.15,
       'Start'
     )
-      .from(
+      t1.from(
         '.bannerMain-lft__inner',
         1,
         { y: 20, opacity: 0, ease: Power2.easeIn },
         1.4
       )
-      .from('.btn-primary', 1, { y: 20, opacity: 0, ease: Power2.easeIn }, 1.4);
+      t1.from('.btn-primary', 1, { y: 20, opacity: 0, ease: Power2.easeIn }, 1.4);
   });
 
   return (
-    <div className="App container">
-      <section className="bannerMain">
-        <div className="bannerMain-lft">
-          <div className="bannerMain-lft__inner">
-          <h2>
-            <span className="text">Go paperless </span><br />
-            <span className="text">with our App</span>
-          </h2>
-          <p className="textAnimation">
-            We provide seemeless integration with <br /> multiple devices so
-            that you can take <br /> notes even better.
-          </p>
+    <div className="App">
+      <nav>
+        <div class="nav-logo">PAPERO .</div>
+        <div className="nav-items">
+          <ul>
+            <li>
+              <a href="javascript.void()">Home</a>
+            </li>
+            <li>
+              <a href="javascript.void()">Products</a>
+            </li>
+            <li>
+              <a href="javascript.void()">Pricing</a>
+            </li>
+            <li>
+              <a href="javascript.void()">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="container">
+        {' '}
+        <section className="bannerMain">
+          <div className="bannerMain-lft">
+            <div className="bannerMain-lft__inner">
+              <h2>
+                <span className="text">Go paperless </span>
+                <br />
+                <span className="text">with our App</span>
+              </h2>
+              <p className="textAnimation">
+                We provide seemeless integration with <br /> multiple devices so
+                that you can take <br /> notes even better.
+              </p>
+            </div>
+            <button class="btn-primary">request access</button>
           </div>
-          <button class="btn-primary">request access</button>
-        </div>
-        <div className="bannerMain-rgt">
-          <img src={doodle} alt="doodle" />
-        </div>
-      </section>
+          <div className="bannerMain-rgt">
+            <img src={doodle} alt="doodle" />
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
